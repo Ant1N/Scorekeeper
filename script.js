b908c2f0bb3c8f9bdca1ef68fc39009cf7bf926d
Ant1N <isakantin@live.se>
2021-02-01 21:41:28 +0100

    finished

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
+var players = []
+var userID = 1
+
+function updateSB() {
+  let element = document.getElementById('parent-list');
+  element.innerHTML = '';
+  players = players.sort((a,b) => {
+    if(a.score > b.score)
+      return -1
+    if (a.score < b.score)
+      return 1
+    return 0 
+  })
+  for (i = 0; i < players.length; i++) {
+    element.innerHTML += "<li class='player' id="+players[i].id+">\
+    <p>"+players[i].name+"</p>\
+    <span onclick='removeScore("+players[i].id+")'><i class='fas fa-minus-circle'></i></span>\
+    <h2 class='score'>"+players[i].score+"</h2>\
+    <span onclick='addScore("+players[i].id+")'><i class='fas fa-plus-circle'></i></span>\
+    </li>"
+  }
+}
+
 function addPlayer() {
-  const playerInfo = document.querySelector(".player"); //Get player class
-  let clone = playerInfo.cloneNode(true); //clone the player class
-  clone.class = "player";
-  playerInfo.before(clone); //Insert player class
-  let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16); //get random color
-  playerInfo.style.backgroundColor = randomColor; //append the random color to the clone
+  players.push({
+    id: userID, 
+    name: 'player' + userID,
+    score: 0
+  })
+  userID++
+  updateSB()
 }
 
-function addScore() {
-  const playerScore = document.querySelector(".score");
-  let number = playerScore.innerHTML;
-  console.log(number)
-  playerScore.innerHTML = number++;
-  playerScore.innerHTML = number;
+function resetGame() {
+  players=[];
+  userID = 1;
+  updateSB();
 }
 
-// function removeScore() {
-//   const playerScore = document.getElementById("score");
-//   let number = score.innerHTML;
-//   number--;
-//   score.innerHTML = number;
-// }
+function addScore(id) {
+  for (i = 0; i < players.length; i++){
+    if(id== players[i].id){
+      players[i].score++
+    }
+  }
+  updateSB()
+}
 
-// const playerScore = document.getElementById("score");
-// let number = score.innerHTML;
-// number++;
-// score.innerHTML = number;
-// console.log(number)
-
-// const playerScore = document.getElementById("score");
-// let number = playerScore.innerHTML;
-// playerScore.innerHTML = number++;
-// score.innerHTML = number;
+function removeScore(id) {
+  for (i = 0; i < players.length; i++){
+    if(id== players[i].id){
+      players[i].score--
+    }
+  }
+  updateSB()
+}
